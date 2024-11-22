ac7b27f17d5ba1a50186557b48e62f25f9938d7f
tom_laptop <tomshabtay75@gmail.com>
2024-11-22 21:06:02 +0200

    basline for the project

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "../headers/sender.h"
+#include "../headers/receiver.h"
+
+
