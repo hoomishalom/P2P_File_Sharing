a8e2522b65eb467ecae039c1634741632ef892b4
tom_laptop <tomshabtay75@gmail.com>
2024-12-17 14:38:43 +0200

    added message queue implemetation

---
+++ headers/message_queue.h
@@ -0,0 +1,64 @@
+#ifndef MESSAGE_QUEUE_H
+#define MESSAGE_QUEUE_H
+    #include <stdlib.h>
+    #include <string.h>
+
+    #define MAX_DATA_SIZE 1024
+
+    typedef struct Message Message;
+
+    typedef struct MessageQueue MessageQueue;
+
+    struct Message{
+        char data[MAX_DATA_SIZE];
+        size_t len;
+        Message *next;
+    };
+
+    struct MessageQueue{
+        Message *head;
+        Message *tail;
+        size_t len;
+    };
+
+
+    /*
+    initializes a queue struct
+
+    @param queue pointer to the queue struct
+    */
+    void queue_init(MessageQueue *queue);
+
+    /*
+    checks if a queue is empty
+
+    @param queue pointer to the queue struct
+    @return 1 if the queue is empty, 0 otherwise
+    */
+    int is_empty(MessageQueue *queue);
+
+    /*
+    deletes a queue and frees all the memory
+
+    @param queue pointer to the queue struct
+    */
+    void del_queue(MessageQueue *queue);
+
+    /*
+    creates a Message struct and adds it to the queue
+
+    @param queue pointer to the queue struct
+    @param data data to be added to the queue
+    @return length of the data
+    */
+    void enqueue(MessageQueue *queue, const char *data, size_t len);
+
+    /*
+    removes and gets the first Message struct from the queue
+
+    @param queue pointer to the queue struct
+    @param data pointer to the data buffer
+    @return length of the data, 0 if queue is empty
+    */
+    size_t dequeue(MessageQueue *queue, char data[]);
+#endif // MESSAGE_QUEUE_H
---
+++ src/message_queue.c
@@ -0,0 +1,70 @@
+#include "../headers/message_queue.h"
+
+
+void queue_init(MessageQueue *queue)
+{
+    queue -> head = NULL;
+    queue -> tail = NULL;
+    queue -> len = 0;
+}
+
+int is_empty(MessageQueue *queue)
+{
+    return queue -> len == 0;
+}
+
+void del_queue(MessageQueue *queue)
+{
+    while (queue -> len > 0)
+    {
+        Message *msg = queue -> head;
+        queue -> head = msg -> next;
+        queue -> len--;
+
+        free(msg);
+    }
+}
+
+
+void enqueue(MessageQueue *queue, const char *data, size_t len)
+{
+    Message *msg = (Message *)malloc(sizeof(Message));
+    
+    strcpy(msg -> data, data);
+    msg -> len = len;
+    msg -> next = NULL;
+
+    if (queue -> len == 0)
+    {
+        queue -> head = msg;
+        queue -> tail = msg;
+        queue -> len++;
+    }
+    else
+    {
+        queue -> tail -> next = msg;
+        queue -> tail = msg;
+        queue -> len++;
+    }
+}
+
+
+size_t dequeue(MessageQueue *queue, char data[])
+{
+    if (queue -> len == 0)
+    {
+        return 0;
+    }
+
+    Message *msg = queue -> head;
+    queue -> head = msg -> next;
+    queue -> len--;
+
+    strcpy(data, msg -> data);
+    size_t len = msg -> len;
+
+    free(msg);
+
+    return len;
+}
+
